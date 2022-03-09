798b3cc94ea4f9fdbb2f976caed0f7e800265a53
Carlos <carlos.henrique248@gmail.com>
2022-03-09 16:34:05 -0300

    Criado botao config e page config

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './trivia.png';
 import './App.css';
 import Login from './pages/Login';
+import Config from './components/Config';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <Login />
+        <Config />
       </header>
     </div>
   );

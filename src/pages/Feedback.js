8f38fcfbda57a513c283f5575ebd504f42180b5e
felipe5215 <felipe.eleotero@gmail.com>
2022-03-12 04:17:14 -0300

    desenvolvendo pagina de feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,16 +1,48 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
+  handleAssertions = (assert) => {
+    const COULD_BE_BETTER = 'Could be better...';
+    const WELL_DONE = 'Well Done!';
+
+    switch (assert) {
+    case 0:
+      return COULD_BE_BETTER;
+    case 1:
+      return COULD_BE_BETTER;
+    case 2:
+      return COULD_BE_BETTER;
+    default:
+      return WELL_DONE;
+    }
+  }
+
   render() {
+    const { assertions } = this.props;
     return (
       <>
         <Header />
-        <h1 data-testid="feedback-text">Feedback</h1>
+        <h1 data-testid="feedback-text">
+          {this.handleAssertions(assertions)}
+
+        </h1>
       </>
     );
   }
 }
 
-export default connect()(Feedback);
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = ({ player: { assertions, name, score } }) => ({
+  assertions,
+  name,
+  score,
+
+});
+
+export default connect(mapStateToProps)(Feedback);

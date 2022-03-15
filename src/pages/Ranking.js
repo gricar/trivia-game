07ad60a36ca49f33c8d629ae4a3b878c61b90d72
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-03-14 21:51:42 -0300

    ranking mostra jogadores por ordem crescente de pontuacao

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { saveRanking, getRanking } from '../services/localStorage';
+import getGravatarUrl from '../services/getGravatarUrls';
 
 class Ranking extends React.Component {
+  renderRanking = () => {
+    const { player } = this.props;
+    saveRanking(player);
+
+    const playersRanking = getRanking();
+    const sortedRanking = playersRanking
+      .sort((playerA, playerB) => playerB.score - playerA.score);
+    return (
+      <div>
+        {
+          sortedRanking.map((person, index) => {
+            const { gravatarEmail, name, score } = person;
+            return (
+              <li key={ index }>
+                <img
+                  src={ getGravatarUrl(gravatarEmail) }
+                  alt="Gravatar"
+                />
+                <p data-testid={ `player-name-${index}` }>{name}</p>
+                <p data-testid={ `player-score-${score}` }>{score}</p>
+              </li>
+            );
+          })
+        }
+      </div>
+    );
+  }
+
   render() {
     const { history } = this.props;
     return (
       <>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <div>
+          <ul>
+            { this.renderRanking() }
+          </ul>
+        </div>
         <button
           data-testid="btn-go-home"
           type="button"
@@ -22,6 +58,11 @@ class Ranking extends React.Component {
 
 Ranking.propTypes = {
   history: PropTypes.objectOf(Object).isRequired,
+  player: PropTypes.shape.isRequired,
 };
 
-export default Ranking;
+const mapStateToProps = ({ player }) => ({
+  player,
+});
+
+export default connect(mapStateToProps)(Ranking);
--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -1,3 +1,13 @@
+export const getRanking = () => {
+  const actualRanking = localStorage.getItem('PLAYER_RANKING');
+  return !actualRanking ? [] : JSON.parse(actualRanking);
+};
+
+export const saveRanking = (playerData) => {
+  const playersList = getRanking();
+  localStorage.setItem('PLAYER_RANKING', JSON.stringify([...playersList, playerData]));
+};
+
 export const saveInLocalStorage = (userToken) => {
   localStorage.setItem('token', userToken);
 };
